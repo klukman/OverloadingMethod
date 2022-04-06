b39cd6cc2717fb9b94f25654ebd281fa62f6829a
klukman <lukman.arisandi@students.amikom.ac.id>
2022-04-06 20:05:27 +0700

    Add project files.

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else if (number1 < number2)
+            {
+                return number1;
+            }
+            return number1;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else if (number1 < number2)
+            {
+                return number1;
+            }
+            return (int)number3; //conver ke int
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 < number2)
+            {
+                return number2;
+            }
+            else if (number1 > number2)
+            {
+                return number1;
+            }
+            return number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            if (number2 < number3)
+            {
+                return (int)number3;
+            }
+            else if (number2 > number3)
+            {
+                return (int)number3;
+            }
+            return number1; //Conver ke int
+        }
+    }
+}
---
+++ LatihanOverloadingMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #3: {0}", number.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #4: {0}", number.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
